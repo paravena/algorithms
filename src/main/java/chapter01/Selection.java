046b2884b2fd7b996325524c29f27555fe8e8a37
Pablo Aravena <paravena74@gmail.com>
2014-07-02 16:39:00 -0300

    Selection sort

---
+++ src/main/java/chapter01/Selection.java
@@ -0,0 +1,36 @@
+package chapter01;
+
+public class Selection {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (a[j].compareTo(a[min]) < 0) min = j;
+            }
+            exchange(a, i, min);
+        }
+    }
+
+    public static void exchange(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void printArray(Object[] arr) {
+        System.out.print("[" + arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            System.out.printf(",%s", arr[i]);
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        printArray(a);
+        sort(a);
+        printArray(a);
+
+    }
+}
