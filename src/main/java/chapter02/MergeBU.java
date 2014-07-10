22fe9bdc8e7290066dea90b65ccbff25f5a99643
Pablo Aravena <paravena74@gmail.com>
2014-07-10 19:46:39 -0300

    Bottom UP Merge sort

---
+++ src/main/java/chapter02/MergeBU.java
@@ -0,0 +1,54 @@
+package chapter02;
+
+public class MergeBU {
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz + sz) {
+            System.out.println("sz : " + sz);
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
+
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (j > hi ) {
+                a[k] = aux[i++];
+            } else if (i > mid) {
+                a[k] = aux[j++];
+            } else if (less(aux[i], aux[j])) {
+                a[k] = aux[i++];
+            } else {
+                a[k] = aux[j++];
+            }
+        }
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    public static void printArray(Object[] arr) {
+        System.out.print("[" + arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            System.out.printf(",%s", arr[i]);
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"E", "E", "X", "I", "G", "M", "R", "Z", "A", "C", "E", "R", "T"};
+        sort(a);
+        printArray(a);
+    }
+}
