9fa5b0940fb9082509308974ee0943f937c195ae
Pablo Aravena <paravena74@gmail.com>
2014-07-30 16:33:07 -0300

    Graphs

--- src/main/java/graphs/Graph.java
+++ src/main/java/graphs/Graph.java
@@ -39,4 +39,28 @@ public class Graph implements Iterable<Vertex> {
     public Integer getNumVertices() {
         return numVertices;
     }
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addVertex("0");
+        g.addVertex("1");
+        g.addVertex("2");
+        g.addVertex("3");
+        g.addVertex("4");
+        g.addVertex("5");
+        g.addEdge("0", "1", 5);
+        g.addEdge("0", "5", 2);
+        g.addEdge("1", "2", 4);
+        g.addEdge("2", "3", 9);
+        g.addEdge("3", "4", 7);
+        g.addEdge("3", "5", 3);
+        g.addEdge("4", "0", 1);
+        g.addEdge("5", "4", 8);
+        g.addEdge("5", "2", 1);
+        for (Vertex v : g) {
+            for (String id : v.getConnections()) {
+                System.out.printf("(%s, %s)\n", v.getId(), id);
+            }
+        }
+    }
 }
