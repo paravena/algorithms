26e5b746fd4f713019a2bb73c5ea840653a1ddf3
quit <exit>
2014-11-11 22:38:55 -0300

    Word chain almost finished

--- src/main/java/wordchain/WordDictionary.java
+++ src/main/java/wordchain/WordDictionary.java
@@ -33,7 +33,7 @@ public class WordDictionary implements Iterable<String> {
     }
 
     private void loadDictionary() throws IOException {
-        InputStream is = getClass().getClassLoader().getResourceAsStream("words-chain.txt");
+        InputStream is = getClass().getClassLoader().getResourceAsStream("words_small.txt");
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String line;
         words = new HashMap<String, Integer>();
