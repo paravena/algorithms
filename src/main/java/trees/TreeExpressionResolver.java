62bb6c8f12c87872ac396865bc3158cea1d90562
quit <exit>
2014-12-07 10:26:31 -0300

    more tree algorithms

--- src/main/java/trees/TreeExpressionResolver.java
+++ src/main/java/trees/TreeExpressionResolver.java
@@ -8,7 +8,9 @@ public class TreeExpressionResolver implements TreeNodeVisitor<String> {
 
     @Override
     public void visit(String element) {
+        System.out.println("visiting " + element);
         if ("+".equals(element) || "-".equals(element) || "*".equals(element) || "/".equals(element)) {
+
             Double operand1 = numbers.pop();
             Double operand2 = numbers.pop();
             if ("+".equals(element)) {
@@ -33,6 +35,16 @@ public class TreeExpressionResolver implements TreeNodeVisitor<String> {
     }
 
     public static void main(String[] args) {
+        /*
+          This tree represents a mathematical operation : (12 + 24) / 3
+
+               /
+             /  \
+           +    3
+          / \
+        12  24
+         */
+
         TreeExpressionResolver resolver = new TreeExpressionResolver();
         TreeNode<String, String> root = new TreeNode<String, String>("/", resolver);
         TreeNode<String, String> add = new TreeNode<String, String>("+", resolver);
