d118ed975ee2ee98afbf6bf640153540f7ccfd87
Pa2o4i9 <pablo_aravena@gap.com>
2015-11-05 10:26:59 -0300

    new algorithm

--- src/main/java/collections/NegativeSubArrays.java
+++ src/main/java/collections/NegativeSubArrays.java
@@ -1,10 +1,13 @@
 package collections;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NegativeSubArrays {
     public static void main(String[] args) {
-        readInput();
+        int[] input = readInput();
+        int count = countNegativeSubArrays(input);
+        System.out.println(count);
     }
 
     public static int[] readInput() {
@@ -22,5 +25,24 @@ public class NegativeSubArrays {
             input[i++] = Integer.valueOf(token);
         }
         return input;
+    }
+
+    private static int countNegativeSubArrays(int[] input) {
+        int count = 0;
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i; j < input.length; j++) {
+                int[] candidate = Arrays.copyOfRange(input, i, j + 1);
+                if (isNegativeSubArray(candidate)) count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isNegativeSubArray(int[] candidate) {
+        int sum = 0;
+        for (int element : candidate) {
+            sum += element;
+        }
+        return sum < 0;
     }
 }
