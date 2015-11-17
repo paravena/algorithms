85a6cee42d13dc68d0cb98b96a74018c4a98c3dd
Pa2o4i9 <pablo_aravena@gap.com>
2015-11-17 16:08:58 -0300

    minimum average waiting time

---
+++ src/main/java/heap/MinimumAverageWaitingTime.java
@@ -0,0 +1,116 @@
+package heap;
+
+import java.io.InputStream;
+import java.util.*;
+
+public class MinimumAverageWaitingTime {
+    private static PriorityQueue<PizzaOrder> pizzaOrders;
+
+    public static void main(String[] args) {
+        readInput(MinimumAverageWaitingTime.class.getClassLoader().getResourceAsStream("minimum_average_waiting_time.txt"));
+        Double average = 0.0;
+        int N = pizzaOrders.size();
+        PriorityQueue<PizzaOrder> cookingQueue = new PriorityQueue<PizzaOrder>(N, new Comparator<PizzaOrder>() {
+            public int compare(PizzaOrder po1, PizzaOrder po2) {
+                return po1.getCookingTime().compareTo(po2.getCookingTime());
+            }
+        });
+        PizzaOrder po = pizzaOrders.poll();
+        Long clock = po.getArrivalTime();
+        po.setCompletionTime(clock + po.getCookingTime());
+        clock += po.getCookingTime();
+        average += po.getWaitingTime();
+
+        while (pizzaOrders.size() > 0) {
+            po = pizzaOrders.poll();
+            if (po.getArrivalTime() <= clock) {
+                cookingQueue.add(po);
+            } else {
+                Map<String, Object> result = calculatePartialAverage(cookingQueue, clock);
+                average += (Double) result.get("average");
+                clock = (Long) result.get("clock");
+            }
+        }
+
+        if (cookingQueue.size() > 0) {
+            average += (Double) calculatePartialAverage(cookingQueue, clock).get("average");
+        }
+
+        average = average / N;
+
+        System.out.println(average.intValue()); // 1418670047
+    }
+
+    private static Map<String, Object> calculatePartialAverage(PriorityQueue<PizzaOrder> cookingQueue, Long clock) {
+        Double partialAverage = 0.0;
+        while (cookingQueue.size() > 0) {
+            PizzaOrder po = cookingQueue.poll();
+            po.setCompletionTime(clock + po.getCookingTime());
+            clock += po.getCookingTime();
+            partialAverage += po.getWaitingTime();
+        }
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("average", partialAverage);
+        result.put("clock", clock);
+        return result;
+    }
+
+    public static void readInput() {
+        readInput(System.in);
+    }
+
+    public static void readInput(InputStream in) {
+        Scanner scan = new Scanner(in);
+        int N = scan.nextInt();
+        pizzaOrders = new PriorityQueue<PizzaOrder>(N, new Comparator<PizzaOrder>() {
+            public int compare(PizzaOrder po1, PizzaOrder po2) {
+                return po1.getArrivalTime().compareTo(po2.getArrivalTime());
+            }
+        });
+        scan.nextLine();
+        for (int i = 0; i < N; i++) {
+            String[] tokens = scan.nextLine().split("\\s");
+            pizzaOrders.add(new PizzaOrder(Long.valueOf(tokens[0]), Long.valueOf(tokens[1])));
+        }
+        scan.close();
+    }
+}
+
+class PizzaOrder {
+    private Long arrivalTime;
+    private Long cookingTime;
+    private Long completionTime;
+
+    public PizzaOrder(Long arrivalTime, Long cookingTime) {
+        this.arrivalTime = arrivalTime;
+        this.cookingTime = cookingTime;
+    }
+
+    public Long getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Long arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Long getCookingTime() {
+        return cookingTime;
+    }
+
+    public void setCookingTime(Long cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public Long getWaitingTime() {
+        return completionTime - arrivalTime;
+    }
+
+    public Long getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime(Long completionTime) {
+        this.completionTime = completionTime;
+    }
+}
